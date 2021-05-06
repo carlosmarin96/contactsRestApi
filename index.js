fe8d6531081321a9867f62f80b1e392bea31b7f0
Carlos Marin <caa7x21@gmail.com>
2021-05-06 15:05:07 -0600

    ADDED WINSTON LOGGER TO HANDLE ERRORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const winston = require('winston');
 const app = express();
 require('dotenv').config();
 const contactsRoute = require('./routes/contacts')
@@ -10,17 +11,34 @@ const PORT = process.env.PORT || 3000
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//create a logger
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.Console({
+            format:winston.format.combine(
+                winston.format.colorize({all:true})
+            )
+        }),
+        new winston.transports.File({ filename: 'error.log', level:'error'})
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: 'exceptions.log'})
+    ]
+});
+
 //routes
 app.use('/api/contacts', contactsRoute);
 
 //Connecting to mongodb atlas
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true}).then(() => {
-    console.log("Connected to monngodb atlas");
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex:true})
+    .then(() => {
+    logger.info("connected to mongoDb atlas");
 }).catch(error => {
-    console.log("Something wrong happened", error)
+    logger.error(error.message);
 })
 
 //Starting the server
 app.listen(PORT,() =>{
-    console.log("Server started at PORT", PORT)
+    logger.info(`Server started at PORT ${PORT}`);
 });
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { connections } = require('mongoose');
 const router = express.Router();
 const {Contact, validateContact} = require('../models/contacts');
 
 //POST: CREATING A NEW CONTACT
 router.post('/', async (req,res) => {
     const error = await validateContact(req.body);
-    if(error.message) res.status(400).send(error.message)
+    if(error.message) res.status(400).send(error.message);
     contact = new Contact({
         name:req.body.name,
         lastname:req.body.lastname,
@@ -14,11 +15,52 @@ router.post('/', async (req,res) => {
         email:req.body.email
     });
 
-    contact.save().then(contact => {
-        res.send(contact);
-    }).catch(error => {
-        res.status(500).send("Contact was not stored in db");
+    contact
+        .save()
+        .then((contact) => {
+            res.send(contact);
+        })
+        .catch((error) => {
+            res.status(500).send("Contact was not stored in db");
+        });
     });
+
+//GET ALL CONTACTS
+router.get("/", (req,res) =>{
+    Contact.find()
+        .then((contacts) => res.send(contacts))
+        .catch((error) => { 
+            res.status(500).send("Something went wrong");
+        });
 });
 
+//GET THE CONTACT BY ID
+router.get("/:contactId", async (req,res) => {
+    const contact = await Contact.findById(req.params.contactId);
+    if(!contact) res.status(404).send("Contact not found");
+    res.send(contact);
+    return; 
+});
+
+//UPDATE CONTACT BASED ON ID
+router.put("/:contactId", async (req, res) => {
+    const updatedContact = await Contact.findByIdAndUpdate(req.params.contactId,{
+        name:req.body.name,
+        lastname:req.body.lastname,
+        company:req.body.company,
+        phone:req.body.phone,
+        email:req.params.email
+    },
+    {new:true}
+    );
+    if(!updatedContact) res.status(404).send("Contact not found");
+});
+
+
+//DELETE BOOK BASED ON ID
+router.delete('/:contactId', async (req,res) =>{
+    const contact = await Contact.findByIdAndRemove(req.params.contactId);
+    if (!contact) res.status(404).send("Contact with id not found");
+    res.send(contact);
+});
 module.exports = router;
